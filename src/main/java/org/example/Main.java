056cdceadfd0f9e7c3288f3ddda5c820556fa55f
Santiago Alarcón <santialfa67@gmail.com>
2025-02-17 19:01:00 -0500

    Otro hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+
     }
 }
